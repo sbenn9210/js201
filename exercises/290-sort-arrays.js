019d21f82a45533756d04e52e6d8d5d3306492c2
sbenn9210 <sbenn9210@gmail.com>
2020-02-16 22:24:52 -0600

    290-sort-arrays

--- exercises/290-sort-arrays.js
+++ exercises/290-sort-arrays.js
@@ -8,7 +8,9 @@
 // alphaSort(['b', 'a', 'c'])
 // > ['a', 'b', 'c']
 
-
+function alphaSort(arr) {
+  return arr.sort();
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "strLengthSort" that sorts an array of strings by how long
@@ -18,8 +20,11 @@
 // strLengthSort(['Apple', 'Banana', 'Cherry'])
 // > ['Apple', 'Cherry', 'Banana']
 
-
-
+function strLengthSort(arr) {
+  return arr.sort(function(a, b) {
+    return a.length - b.length;
+  });
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "sumSort". Given an array of array of numbers like:
 // var arr = [
@@ -38,3 +43,9 @@
 //   [4, 5]
 // ])
 // > [[2], [4, 5], [9, 1, 9]]
+
+function sumSort(arr) {
+  return arr.sort((a, b) => {
+    return a - b;
+  });
+}
